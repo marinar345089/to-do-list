a94a4ed186986f23976b61b0eb8a0788b213f2b3
marinar345089 <marina2000ruchworld@gmail.com>
2025-04-27 13:45:26 +0400

    complete

--- script.js
+++ script.js
@@ -8,32 +8,44 @@ const emptyTemplate = document.querySelector(".empty__template");
 const modalInput = document.querySelector(".modal__input");
 const applyBtn = document.querySelector(".modal__apply");
 const select = document.querySelector(".search__select");
+const searchInput = document.querySelector(".search__input");
+const modalTitle = document.querySelector(".modal__title");
 
-let todoList = [
-  /* {
+let todoList = getList();
+
+let editTodo = null;
+/* {
     id: 1,
     text: "note#1",
     complete: false,
   },*/
-];
 
 function openModal() {
   modal.classList.add("modal__open");
+  if (editTodo) {
+    modalTitle.textContent = "EDIT NOTE";
+    modalInput.value = editTodo.text;
+  } else {
+    modalTitle.textContent = "NEW NOTE";
+    modalInput.value = "";
+  }
 }
 
 function closeModal() {
   modal.classList.remove("modal__open");
+  editTodo = null;
 }
 
 function toggleTheme() {
   document.body.classList.toggle("dark");
 }
 
-function renderList() {
+function renderList(array) {
+  saveList();
   // обнуляем содержимое лист, чтобы не происходило дублирования разметки
   list.innerHTML = null;
   // позволяет выполнить функцию для каждого элемента внутри массива
-  todoList.forEach(function (todo) {
+  array.forEach(function (todo) {
     console.log(todo);
     // универ. запись, к. позволяет скопировать всю разметку внутри элемента template
     const clone = listTemplate.content.cloneNode(true);
@@ -42,6 +54,11 @@ function renderList() {
     const listTodo = clone.querySelector(".list__todo");
     const btnDelete = clone.querySelector(".list__delete");
     const btnCheck = clone.querySelector(".list__check");
+    const btnEdit = clone.querySelector(".list__edit");
+
+    btnEdit.onclick = function () {
+      startEdit(todo.id);
+    };
 
     btnDelete.onclick = function () {
       // если функци принимает параметры, при назначении её по клику необходимо сделать функцию-обёртку
@@ -59,7 +76,7 @@ function renderList() {
     // добавляет html разметку внутрь и в конец элемента (лист)
     list.append(clone);
   });
-  if (todoList.length === 0) {
+  if (array.length === 0) {
     const clone = emptyTemplate.content.cloneNode(true);
     list.append(clone);
   }
@@ -70,6 +87,16 @@ function addTodo() {
   const text = modalInput.value;
   if (text === "") {
     alert("Enter a task");
+    return;
+  }
+  if (editTodo) {
+    // мэп позволяет преобр. элементы массива. результатом возвращает новый изменённый массив.
+    todoList = todoList.map(function (todo) {
+      if (todo.id === editTodo.id) {
+        todo.text = text;
+      }
+      return todo;
+    });
   } else {
     //добавляются объекты в конец массива
     todoList.push({
@@ -78,12 +105,12 @@ function addTodo() {
       text: text,
       complete: false,
     });
-    //отрисовать обновл. список
-    renderList();
-    closeModal();
     //почистили поле для ввода
     modalInput.value = "";
   }
+  //отрисовать обновл. список
+  renderList(todoList);
+  closeModal();
 }
 // функция параметром принимает id объекта, который надо удалить
 function deleteTodo(id) {
@@ -92,7 +119,7 @@ function deleteTodo(id) {
     // оставляем только те элементы, id которых не равно id объекта для удаления
     return todo.id != id;
   });
-  renderList();
+  renderList(todoList);
 }
 
 function completeTodo(id) {
@@ -102,29 +129,60 @@ function completeTodo(id) {
   });
   //присваиваем противоположное значение (если тру, то фолс и наоборот)
   currentTodo.complete = !currentTodo.complete;
-  renderList();
+  renderList(todoList);
 }
 
 function filterList() {
   const value = select.value;
+  let result = todoList;
   if (value === "incomplete") {
-    const result = todoList.filter(function (todo) {
+    result = todoList.filter(function (todo) {
       return todo.complete === false;
     });
   } else if (value === "complete") {
-    const result = todoList.filter(function (todo) {
+    result = todoList.filter(function (todo) {
       return todo.complete === true;
     });
-  } else {
   }
+  renderList(result);
+}
+
+function handleSearch() {
+  const value = searchInput.value;
+  const result = todoList.filter(function (todo) {
+    return todo.text.toLowerCase().includes(value.toLowerCase());
+  });
+  renderList(result);
+}
+
+function saveList() {
+  const list = JSON.stringify(todoList);
+  localStorage.setItem("todoList", list);
+}
+
+function getList() {
+  const list = localStorage.getItem("todoList");
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+}
+
+function startEdit(id) {
+  editTodo = todoList.find(function (todo) {
+    return todo.id === id;
+  });
+  openModal();
 }
 
 crossBtn.onclick = openModal;
 cancelBtn.onclick = closeModal;
 switchBtn.onclick = toggleTheme;
 applyBtn.onclick = addTodo;
-
-renderList();
+select.onchange = filterList;
+searchInput.oninput = handleSearch;
+renderList(todoList);
 /*
 function sum(a, b) {
   console.log(a + b);
